62ecc06d4d4761fa187ef1b646c3daf92981a021
airton-sudo <73069455+airton-sudo@users.noreply.github.com>
2023-12-06 02:07:10 +0000

    Fix: Banner

---
+++ app/components/Banner.tsx
@@ -0,0 +1,21 @@
+export default  function Banner() {
+    return (
+        <div>
+            <div className="md:flex items-center flex-row-reverse">
+                <div className="basis-1/2">
+                                    </div>
+                <div className="basis-1/2 text-center text-white">
+                    <h2>Sistema de controle de alunos</h2>
+                    <h3>Facilitando a gestão</h3>
+                    <a
+                        className="text-[#3d3d3d] bg-white px-2 py-[4px] rounded hover:bg-[#00e77f] hover:text-white"
+                        href="#"
+                    >
+                        Informações
+                    </a>
+                </div>
+            </div>
+
+        </div>
+    )
+}
