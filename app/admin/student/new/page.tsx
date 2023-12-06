7ccecd0eaba916fe912226afe73f2a1cfa991cd8
airton-sudo <73069455+airton-sudo@users.noreply.github.com>
2023-12-06 01:50:56 +0000

    fix: cadastro

--- app/admin/student/new/page.tsx
+++ app/admin/student/new/page.tsx
@@ -25,13 +25,13 @@ export default function NewStudent({
     }
     return (
         <div>
-            <h1 className=" text-center text-4xl">Cadastrar Student</h1>
+            <h1 className=" text-center text-4xl">Cadastrar aluno</h1>
             <form>
                 <Input type="text" name="name" placeholder="Digite o nome do aluno"/><br/>
                 <Input type="text" name="email" placeholder="Digite o email do aluno"/> <br/>
                 <br/>
                 <UploadButton /> <br/>
-                <Button formAction={saveStudent} className="text-white">Salvar</Button>
+                <Button formAction={savestudent} className="text-white">Salvar</Button>
             </form>
         </div>
 
